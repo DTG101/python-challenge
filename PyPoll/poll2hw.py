05930482723bcc2aae4a729cfe9a46aeaa3affb5
dylan grimm <dylangrimm12@gmail.com>
2018-10-04 19:54:36 -0500

    Final

--- PyPoll/poll2hw.py
+++
@@ -1,9 +0,0 @@
-# Dependencies
-import pandas as pd 
-    
-# Save path to data set in a variable
-data_file = "electiondata.csv" 
-
-# Use Pandas to read data
-data_file_pd = pd.read_csv(data_file)
-data_file_pd.head()
